16fd96afe1444797714e98fdf71e790c91342736
unknown <guiniquegui@gmail.com>
2021-01-26 01:17:50 -0300

    inicio add firebase

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/firestore'
+
+// firebase init - add your own config here
+const firebaseConfig = {
+    apiKey: "AIzaSyDeOJ6djSF1MjghUXZ0vHKn0-RXNy8jf30",
+    authDomain: "sitemoveis-fff9d.firebaseapp.com",
+    projectId: "sitemoveis-fff9d",
+    storageBucket: "sitemoveis-fff9d.appspot.com",
+    messagingSenderId: "710590500020",
+    appId: "1:710590500020:web:8d6748241ae09743759be1",
+    measurementId: "G-PW40M5MZ9R"
+}
+firebase.initializeApp(firebaseConfig)
+
+// utils
+const db = firebase.firestore()
+const auth = firebase.auth()
+
+// collection references
+const usersCollection = db.collection('users')
+const postsCollection = db.collection('posts')
+const commentsCollection = db.collection('comments')
+const likesCollection = db.collection('likes')
+
+// export utils/refs
+export {
+  db,
+  auth,
+  usersCollection,
+  postsCollection,
+  commentsCollection,
+  likesCollection
+}
--- src/main.js
+++ src/main.js
@@ -3,12 +3,18 @@ import App from "./App.vue";
 import SuiVue from 'semantic-ui-vue';
 import 'semantic-ui-css/semantic.min.css';
 import router from './router'
+import {auth} from './firebase.js'
 
 Vue.use(SuiVue);
 Vue.config.ignoredElements = ['model-viewer']
 
-new Vue({
-  router,
-  render: (h) => h(App)
-}).$mount("#app");
+let app
+auth.onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
 
