6475e1bf30b4db1a8d34c5a015f5adb0d5cd1b48
guinique <guiniquegui@gmail.com>
2021-02-12 00:49:27 -0300

    cards etc

--- src/components/Visualizar3D.vue
+++ src/components/Visualizar3D.vue
@@ -4,16 +4,43 @@
             <span>Utilize o menu para visualizar os modelos 3D disponíveis</span>
         </div>
 
+
         <div id="container-modelos">
             <div v-for="(model,i) of models" :key="i" class="modelos">
-                <model-viewer :src="model" auto-rotate camera-controls @click="openModal(model)"></model-viewer>
+            <sui-card >
+                <model-viewer class="modelo3D-card" data-js-focus-visible :src="model" auto-rotate @click="openModal(model)"></model-viewer>
+                <sui-card-content class="cards">
+                    <sui-card-header>{{filtraNome(models[i][1])}}</sui-card-header>
+                    <!-- <sui-card-meta>Joined in 2013</sui-card-meta> -->
+                    <!-- <sui-card-description>Kristy is an art director living in New York.</sui-card-description> -->
+                </sui-card-content>
+            </sui-card>
+
             </div>
         </div>
 
         <!-- Modal -->
         <sui-modal v-model="open">
-            <sui-modal-content class="container-modal-modelo">
-                <model-viewer :src="source" auto-rotate camera-controls class="modal-modelo"></model-viewer>
+            <sui-modal-content   :style="'background-color:'+corBg">
+                <div class="container-modal-modelo">
+                    <model-viewer :skybox-image="hdr" data-js-focus-visible style="width:100%;height:500px" :src="source" camera-controls class="modal-modelo"></model-viewer>
+                </div>
+            </sui-modal-content>
+            <sui-modal-content extra>
+                <div class="container-fundo">
+                    <div class="container-cores-fundo">
+                        <span>Cor fundo</span>
+                        <sui-button circular color="red" @click="changeBg($event.target.innerHTML)" content="Red" />
+                        <sui-button circular color="orange" @click="changeBg($event.target.innerHTML)" content="Orange" />
+                        <sui-button circular color="yellow" @click="changeBg($event.target.innerHTML)" content="Yellow" />
+                    </div>
+                    <div class="container-hdr-fundo">
+                        <span>HDR</span>
+                        <img class="btn-hdr" :src="iconsHDR[0]" @click="changeHDR('hdr1.hdr')" >
+                        <img class="btn-hdr" :src="iconsHDR[1]" @click="changeHDR('hdr2.hdr')" >
+                        <img class="btn-hdr" :src="iconsHDR[2]" @click="changeHDR('hdr3.hdr')" >
+                    </div>
+                </div>
             </sui-modal-content>
         </sui-modal>
 
@@ -31,12 +58,41 @@ export default {
             models:[],
             open:false,
             source: null,
+            corBg: "lightgray",
+            iconsHDR: [],
+            hdr:null
         }
     },
     mounted(){
-        this.getImg()
+        this.getImg(),
+        this.getHDR()
     },
     methods:{
+
+        changeHDR(hdr){
+            var storage = firebase.storage()
+
+            var storageRef = storage.ref()
+
+
+            storageRef.child('HDR/'+hdr).getDownloadURL().then((url) =>{
+                console.log(url)
+                this.hdr = url
+            }).catch(function(e){
+                console.log(e)
+            })
+
+            
+        },
+
+        filtraNome(nome){
+            return nome.replace(".glb","").replaceAll("_"," ")
+        },
+
+        changeBg(cor){
+            this.corBg = cor
+        },
+
         openModal(url){
             this.source = url
             this.open = !this.open;
@@ -53,34 +109,89 @@ export default {
             let listaItems = await listRef.listAll()
             for(let item of listaItems.items){
                 let url = await item.getDownloadURL()
-                this.models.push(url)
+                let nome = await item.name
+                this.models.push([url,nome])
             }
+        },
 
-        }
+        async getHDR(){
+            var storage = firebase.storage()
+
+            // Create a storage reference from our storage service
+            var storageRef = storage.ref()
+
+            var  listRef = storageRef.child('HDR/Icones')
+
+            let listaItems = await listRef.listAll()
+
+            for(let item of listaItems.items){
+                let imgURL = await item.getDownloadURL()
+                this.iconsHDR.push(imgURL)
+            }
+        },
     }
 }
 </script>
 
 <style scoped>
+    .cards{
+        background-color: #1b1b1b !important ;
+    }
+
+    .cards>*{
+        color:white !important;
+    }
+
+    .container-fundo{
+        /* border:2px solid red; */
+        display:flex;
+        justify-content: left;
+    }
+
+    .container-cores-fundo{
+        /* border:2px solid red; */
+        width:50%;
+        display: flex;
+        justify-content: space-evenly;
+        line-height: 50px;
+    }
+    .modelo3D-card{
+        width: 100%;
+        cursor: pointer;
+    }
+
     #container-modelos{
         display:flex;
         flex-wrap: wrap;
-        justify-content: space-evenly;
-        padding: 40px;
+        /* justify-content: space-between; */
+        margin: 40px;
     }
+
     .modelos{
-        border:2px solid red;
-        margin:5px;
+        margin:10px;
     }
+
     .container-modal-modelo{
         display: flex;
-        justify-content:center;
-        border: 2px solid red;
-        
+        justify-content: center;
     }
+
     .modal-modelo{
-        border: 2px solid blue;
+        /* border: 2px solid blue; */
     }
+
+    .btn-hdr{
+        cursor: pointer;
+        width: 50px;
+    }
+
+    .container-hdr-fundo{
+        line-height: 50px;
+        width: 50%;
+        display:flex;
+        justify-content: space-evenly;
+    }
+
     #txt{
         margin-top: 20px;
         /* border: 2px solid red; */
